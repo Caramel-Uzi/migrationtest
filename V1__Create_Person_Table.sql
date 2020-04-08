7631c21d7ba2ae4937bdb87876ff066d35d320c1
unknown <uzair.tariq@revolvinggames.com>
2020-04-08 15:48:43 +0500

    Test Migrations

--- V1__Create_Person_Table.sql
+++ V1__Create_Person_Table.sql
@@ -1,6 +1,12 @@
-CREATE TABLE Persons (
-    PersonID int,
-    LastName varchar(255),
-    FirstName varchar(255),
-    version int NOT NULL
+CREATE TABLE IF NOT EXISTS snapshot (
+  persistence_id VARCHAR(255) NOT NULL,
+  sequence_nr BIGINT NOT NULL,
+  created_at BIGINT NOT NULL,
+  manifest VARCHAR(255) NOT NULL,
+  snapshot LONGTEXT NOT NULL,
+  serializer_id INT,
+  version BIGINT NOT NULL,
+  PRIMARY KEY (persistence_id, sequence_nr),
+  INDEX snapshot_sequence_nr_idx (sequence_nr),
+  INDEX snapshot_created_at_idx (created_at)
 );
--- V2__Insert_Into_Person_Table.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO Persons(PersonID,FirstName,LastName,version) VALUES (1,'Uzair','Tariq',1);
-INSERT INTO Persons(PersonID,FirstName,LastName,version) VALUES (2,'Rizwan','Tariq',1);
