45eb5a4572657bedb828cd1f1550c7541acd30ea
unknown <uzair.tariq@revolvinggames.com>
2020-04-08 15:24:33 +0500

    Small Fix

--- V2__Insert_Into_Person_Table.sql
+++ V2__Insert_Into_Person_Table.sql
@@ -1,2 +1,2 @@
-INSERT INTO persons(PersonID,FirstName,LastName,version) VALUES (1,'Uzair','Tariq',1);
-INSERT INTO persons(PersonID,FirstName,LastName,version) VALUES (2,'Rizwan','Tariq',1);
+INSERT INTO Persons(PersonID,FirstName,LastName,version) VALUES (1,'Uzair','Tariq',1);
+INSERT INTO Persons(PersonID,FirstName,LastName,version) VALUES (2,'Rizwan','Tariq',1);
